ddb254c681cc1abd4150f251f0aa151eb4a23c31
Tomasz <tomgaek@gmail.com>
2024-01-29 16:59:09 +0000

    set up vue and storybook

---
+++ src/components/container.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container" id="listContainer">
+        <ul class="container__list">
+        </ul>
+        <form class="container__form" action="">
+            <textarea name="textarea" class="container__textarea" id="" cols="30" rows="10"></textarea>
+            <button class="container__btn" type="button">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+</script>
