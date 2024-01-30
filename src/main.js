426f6f0989eb2777daea5646657ddbe4a63b2c63
Tomasz <tomgaek@gmail.com>
2024-01-30 01:22:14 +0000

    Add scss plugin for vite and create simple scss files to test functionality

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './main.scss';
+
 import {createApp} from 'vue';
 import App from './App.vue';
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vitePluginSass from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        vitePluginSass(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+});
