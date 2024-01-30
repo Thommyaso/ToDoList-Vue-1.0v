53180c706e1146426baa82b8903bf57ae71769ca
Tomasz <tomgaek@gmail.com>
2024-01-30 09:58:15 +0000

    Replace vite-plugin-sass with sass package

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ import {fileURLToPath, URL} from 'node:url';
 
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vitePluginSass from 'vite-plugin-sass';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        vitePluginSass(),
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            sass: {
+                implementation: sass,
+            },
         },
     },
 });
